9af121f4848d1b20526964d75f56844779b58b90
Ana Clara <ana4ew.br@gmail.com>
2024-09-11 16:49:46 -0300

    Fixed: Javascript Function

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $('.edit-button').on('click', function () {
-        var task = $(this).closest('.task');
+        var $task = $(this).closest('.task');
 
-        task.find('.progress').addClass('hidden');
+        $task.find('.progress').addClass('hidden');
 
-        task.find('.task-description').addClass('hidden');
+        $task.find('.task-description').addClass('hidden');
 
-        task.find('.task-actions').addClass('hidden');
+        $task.find('.task-actions').addClass('hidden');
 
-        task.find('.edit-task').removeClass('hidden');
+        $task.find('.edit-task').removeClass('hidden');
     });
 
     $('.progress').on('click', function() {
